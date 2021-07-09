52ac8674fb5ffa0d898a6793fc3276bbea163169
LuanBarreto <luaanr@hotmail.com>
2021-07-09 20:02:56 -0300

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,13 +5,27 @@ for (let i = 0; i < mashPotatoes.length; i++) {
   //addFood(steak[i], '#steak');
   console.log(mashPotatoes[i])
 }
-
 // Iteration 1 using callbacks
 addFood(steak[0], '#steak', () => {
   // ... your code here
   addFood(steak[1], '#steak', () => {
 
   })
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+                console.log('steak is done');
+                document.querySelector('#table').innerHTML += '<img src="/public/images/steak.jpg">'
+              })
+            })
+          })
+        })
+      })
+  }) 
+})
 });
 
 
@@ -20,12 +34,35 @@ addFood(steak[0], '#steak', () => {
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
   // ... your code here
   addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+          document.querySelector('#table').innerHTML += '<img src="/public/images/mashPotatoes.jpg">'
+
+    })
+  })
+})
+})
 });
 
 // Iteration 3 using async/await
 
   async function makeFood(step) {
+  async function makeFood(steps, id) {
     // ... your code here
-    
+
+    for (let step of steps) {
+      await addFood(step,id)
+    }
+    document.querySelector('#table').innerHTML += '<img src="/public/images/brusselSprouts.jpg>'
   }
   makeFood(eachStep);
+
+  Promise.all([
+  makeFood(brusselSprouts, '#brusselSprouts'),
+  makeFood(steak, '#steak'),
+  makeFood(mashPotatoes, '#mashPotatoes'),
+  ]).then (() => {
+    document.body.innerHTML += '<button>Dinner is served</button>';
+  }); 
